9321f858a58cc68f6bd296692503dbac0a757017
Shawn chiu <shawn20111207@gmail.com>
2024-08-08 17:46:34 +0800

    add for loop

---
+++ products.py
@@ -0,0 +1,9 @@
+products = []
+while True:
+	name = input('請輸入商品名稱: ')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格: ')
+	products.append ([name, price])
+for p in products:
+	print(p[0], '的商品價格是', p[1])
