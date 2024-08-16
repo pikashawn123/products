7ef6149428d9be3dc00bb2c0288a343655ad2160
Shawn chiu <shawn20111207@gmail.com>
2024-08-16 19:21:08 +0800

    add read file

--- products.py
+++ products.py
@@ -1,9 +1,28 @@
 products = []
+
+# 讀取檔案的程式
+with open ('products.csv', 'r') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name, price])
+	print(products)
+
+# 能讓使用者輸入的程式
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
 		break
 	price = input('請輸入商品價格: ')
 	products.append ([name, price])
+
+# 印出購買紀錄的程式
 for p in products:
 	print(p[0], '的商品價格是', p[1])
+
+# 寫入檔案的程式
+with open ('products.csv', 'w') as f:
+	f.write('商品' + ',' + '價格' + '\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
